c2221441843e23d897b1e83b2b23a2f83a7ee2c1
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2022-11-24 18:05:43 +0200

    Screenshot code and gui

---
+++ screenshot.py
@@ -0,0 +1,26 @@
+import time
+import tkinter as tk
+
+import pyautogui as pag
+
+
+
+def screenshot():
+    name= int(round(time.time())*1000) #get the time and round it up
+    name = 'D:/Python Projectss/ScreenShot-App/Screenshots Images/{}.png'.format(name) #get the random number we generate it and pass it in the {}
+    time.sleep(3) #wait for a while before taking a screenshot
+    img = pag.screenshot(name) #built-in function to take screenshots
+    img.show()
+
+
+
+
+#creates a screen
+
+r = tk.Tk()
+button1= tk.Button(r, text="Take Screenshot", command=screenshot)
+button1.pack()
+button2= tk.Button(r, text="Quit",command=quit)
+button2.pack()
+
+r.mainloop()
